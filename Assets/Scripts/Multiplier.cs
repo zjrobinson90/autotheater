e60d025e876c0f4aa52c8cb6e72917914bc6e0b0
Zechariah Robinson <zjrobinson90@gmail.com>
2017-11-19 13:56:38 -0800

    added Micah's models

---
+++ Assets/Scripts/Multiplier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiplier : MonoBehaviour {
+
+    public Transform prefab;
+    void Start(){
+        for (int i = 0; i < 300; i++){
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
